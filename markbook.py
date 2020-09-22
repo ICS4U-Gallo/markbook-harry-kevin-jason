6ee2b9d140bd27e583f52caf28ab4974a08717d3
MorikamiForest <chengzong.zhao@gmail.com>
2020-09-22 13:59:41 -0400

    By Chengzong 1:59 Sept 22

--- markbook.py
+++ markbook.py
@@ -1,6 +1,6 @@
 """
 Markbook Application
-Group members: 
+Group members: Chengzong Jason Harry
 """
 from typing import Dict
 
@@ -15,17 +15,22 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
     Returns:
         Assignment as a dictionary.
     """
-    return {}
+    return {'name': name, 'due': due, 'points': points}
 
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     """Creates a classroom dictionary"""
-    return {}
+    return {'course_code': course_code, 'course_name': course_name, 'period': period, 'teacher':teacher, 'student_list': [], 'assignment_list': []}
 
 
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
-    return 0
+    total = 0
+    for mark in student['marks']:
+        total += mark
+    average = total/len(student['marks'])
+
+    return average
 
 
 def add_student_to_classroom(student: Dict, classroom: Dict):
@@ -35,7 +40,7 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
         student: Student dict
         classroom: The classroom to add the student to
     """
-    pass
+    return classroom['student_list'].append(student)
 
 
 def remove_student_from_classroom(student: Dict, classroom: Dict):
@@ -45,7 +50,10 @@ def remove_student_from_classroom(student: Dict, classroom: Dict):
         student: The student to be removed
         classroom: the class from which the student will be removed.
     """
-    pass
+    try:
+        return classroom['student_list'].remove(student)
+    except:
+        return classroom
 
 
 def edit_student(student: Dict, **kwargs: Dict):
@@ -57,4 +65,4 @@ def edit_student(student: Dict, **kwargs: Dict):
             data that needs to be changed. Can come in the form
             of a dictionary.
     """
-    pass
+    return student.update(kwargs)
---
+++ test.py
@@ -0,0 +1,8 @@
+from test_markbook import *
+
+test_create_assigment()
+test_create_classroom()
+test_add_student_to_classroom()
+test_calculate_average_mark()
+test_edit_student()
+test_remove_student_from_classroom()
